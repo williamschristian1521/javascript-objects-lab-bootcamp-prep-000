2caa498a586ea0c34bec4144a8e7fd876a3b526b
Christian Williams <williamschristian1521@gmail.com>
2018-04-19 01:19:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,10 @@ function updateObjectWithKeyAndValue(object, key, value)
   var newObject = Object.assign({}, object, {[key] : value})
   return newObject
 }
+
+function deleteFromObjectByKey(object, key)
+{
+  var newObject = Object.assign({}, object)
+  delete newObject[key]
+  return newObject
+}
