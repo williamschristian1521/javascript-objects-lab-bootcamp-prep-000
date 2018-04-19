d77c55e8a6f45560043b2fd084ab3e7743adc4bc
Christian Williams <williamschristian1521@gmail.com>
2018-04-19 01:22:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,3 +18,9 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value)
   object[key] = value
   return object
 }
+
+function destructivelyDeleteFromObjectByKey(object, key)
+{
+  delete object[key]
+  return object
+}
